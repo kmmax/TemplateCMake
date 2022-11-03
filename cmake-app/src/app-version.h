a3574477e956db84242e00cfce85d4434884b705
kmmax <kmmax@yandex.ru>
2022-11-03 17:28:09 +0300

    install target is added

---
+++ cmake-app/src/app-version.h
@@ -0,0 +1,29 @@
+#ifndef APP_VERSION_H
+#define APP_VERSION_H
+
+ /* The name of project */
+#define APP_NAME "CMakeTemplate"
+
+ /* The major version, (1, if %APP_VERSION is 1.2.3) */
+#define APP_VERSION_MAJOR 1
+
+ /* The minor version (2, if %APP_VERSION is 1.2.3) */
+#define APP_VERSION_MINOR 2
+
+ /* The micro version (3, if %APP_VERSION is 1.2.3) */
+#define APP_VERSION_BUILD 8
+
+ /* The full version, like 1.2.3 */
+#define APP_VERSION        
+
+ /* The full version, in string form (suited for string concatenation) */
+#define APP_VERSION_STRING ""
+
+/* Shot description of project */
+#define APP_DESCRIPTION 	"This is template of С++ project"
+
+/* URL of site homepage */
+#define APP_HOMEPAGE_URL 	"https://example.com"
+
+
+#endif /* APP_VERSION_H */
---
+++ cmake-app/src/defines.h
@@ -0,0 +1,3 @@
+#define APP_VAR1 100
+#define APP_VAR2 100.0
+#define APP_VAR3 "One hundred"
